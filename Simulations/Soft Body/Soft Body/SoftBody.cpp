0150f6af04c3f02fd1c4642bd883d6cf3886577c
Trevor Graham <trevor.graham2121@gmail.com>
2023-10-05 13:00:36 -0500

    Soft Body Sim

---
+++ Simulations/Soft Body/Soft Body/SoftBody.cpp
@@ -0,0 +1,13 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+int main()
+{
+    /// ADD camera and shader class to this project
+}
+
